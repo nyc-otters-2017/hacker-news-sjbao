0f3566c7b357fa886f5c443345db7be7a8ecda9e
sjBao <supuilam@gmail.com>
2017-01-24 20:47:00 -0500

    Complete user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/'
+    else
+      render :new
+    end
+
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :email, :username, :password, { :presence => true }
   validates :email, :username, { :uniqueness => true }
+  validates :password, length: { minimum: 6 }
   validates_confirmation_of :password
   has_secure_password
 end
